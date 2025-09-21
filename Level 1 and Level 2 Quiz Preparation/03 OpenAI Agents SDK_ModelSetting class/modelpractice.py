71c8784c37e60ad71578f05b835dd460b120f65c
Asiya Khan <160250124+asiyakhan990@users.noreply.github.com>
2025-09-21 16:43:49 +0500

    first Commit

--- Level 1 and Level 2 Quiz Preparation/03 OpenAI Agents SDK_ModelSetting class/modelpractice.py
+++ Level 1 and Level 2 Quiz Preparation/03 OpenAI Agents SDK_ModelSetting class/modelpractice.py
@@ -1,4 +1,5 @@
 from agents import Agent, Runner, ModelSettings
+from openai.types import Reasoning
 from dotenv  import load_dotenv
 
 load_dotenv()
@@ -11,16 +12,24 @@ agent = Agent(
         # top_p=0.0,
         # frequency_penalty=1.0,
         # presence_penalty=2.0,
-        # truncation="auto"
+        # truncation="auto",
+        # max_tokens=25,
+        reasoning=Reasoning(
+            # effort=["medium"]
+            summary=["concise"]
+        )
     ),
-    model="gpt-3.5-turbo"
+    # model="gpt-3.5-turbo"
+    model="gpt-5",
+    
 )
 
 # sentence = "how are you" * 17000
 
 result = Runner.run_sync(
     agent,
-    "Write a essay on pakistan in 17000 words"
+    # "Write a essay on pakistan in 17000 words"
+    "What is Python"
 )
 
 print(result.final_output)
