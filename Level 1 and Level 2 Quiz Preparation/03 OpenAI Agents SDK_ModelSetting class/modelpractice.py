9647c2d32b4545038e0808216d4192871a24382d
Asiya Khan <160250124+asiyakhan990@users.noreply.github.com>
2025-09-21 05:34:22 +0500

    first Commit

--- Level 1 and Level 2 Quiz Preparation/03 OpenAI Agents SDK_ModelSetting class/modelpractice.py
+++ Level 1 and Level 2 Quiz Preparation/03 OpenAI Agents SDK_ModelSetting class/modelpractice.py
@@ -7,20 +7,20 @@ agent = Agent(
     name="Assistant",
     instructions="You are a helpful assistant.",
     model_settings=ModelSettings(
-        temperature=0.1,
-        top_p=0.0,
-        frequency_penalty=1.0,
-        presence_penalty=2.0,
-        truncation="auto"
+        # temperature=0.1,
+        # top_p=0.0,
+        # frequency_penalty=1.0,
+        # presence_penalty=2.0,
+        # truncation="auto"
     ),
     model="gpt-3.5-turbo"
 )
 
-sentence = "how are you" * 17000
+# sentence = "how are you" * 17000
 
 result = Runner.run_sync(
     agent,
-    sentence
+    "Write a essay on pakistan in 17000 words"
 )
 
 print(result.final_output)
